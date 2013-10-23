9f7ddd913e96934032aa8a5187ce1f336a563d4c
Ryan Holmes <holmes.ryan.90@gmail.com>
2013-10-23 16:12:34 +0000

    fixed path typo

--- head.php
+++ head.php
@@ -2,10 +2,10 @@
 <head>
     <title><?php echo $title; ?> | Gas Sites</title>
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
-    <link href="<?php echo BASE_PATH; ?>/static/style.css" rel="stylesheet">
+    <link href="<?php echo BASE_PATH; ?>static/style.css" rel="stylesheet">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
-    <script src="<?php echo BASE_PATH; ?>/static/jquery.dataTables.min.js"></script>
+    <script src="<?php echo BASE_PATH; ?>static/jquery.dataTables.min.js"></script>
 </head>
 <body>
 <div class='wrapper'>
